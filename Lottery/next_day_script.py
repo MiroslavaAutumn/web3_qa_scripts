16ee5dae63dc5908d09ddd0959fcf568b8787feb
miroslavaautunm <miroslavaautumn@gmail.com>
2023-01-18 17:48:52 +0300

    fix for the first run when fakeTimestamp == 0

--- Lottery/next_day_script.py
+++ Lottery/next_day_script.py
@@ -1,3 +1,4 @@
+import time
 import Lottery.preset
 from Lottery.preset import sign_send_tx, web3_init, get_contract_abi
 
@@ -13,18 +14,32 @@ def lottery_init():
 
 def change_fake_time():
     lottery_contract, web3 = lottery_init()
+    web3 = web3_init()
 
-    change_fake_time_listen = lottery_contract.functions.changeFakeTimeListen()
-    tx_hash = sign_send_tx(web3, change_fake_time_listen, Lottery.preset.OWNER,
-                           Lottery.preset.OWNER_PRIV)
-    print('switch', 'https://testnet.bscscan.com/tx/' + tx_hash.hex())
+    get_fake_time_status = lottery_contract.functions.fakeTimeListen().call()
+    print(get_fake_time_status)
+
+    if get_fake_time_status == False:
+        change_fake_time_listen = lottery_contract.functions.changeFakeTimeListen()
+        tx_hash = sign_send_tx(web3, change_fake_time_listen, Lottery.preset.OWNER,
+                               Lottery.preset.OWNER_PRIV)
+        print('switch', 'https://testnet.bscscan.com/tx/' + tx_hash.hex())
+    else:
+        print('Status is already true')
 
 
 def set_next_day():
     lottery_contract, web3 = lottery_init()
 
     get_current_timestamp = lottery_contract.functions.fakeTimestamp().call()
-    next_day = int(get_current_timestamp) + 86400
+    print(get_current_timestamp)
+    if get_current_timestamp == 0:
+        ts_now = int(time.time())
+        next_day = ts_now + 86400
+        print(next_day)
+    else:
+        next_day = int(get_current_timestamp) + 86400
+        print('koklel')
 
     change_fake_timestamp = lottery_contract.functions.changeFakeTimestamp(
         next_day
@@ -35,5 +50,5 @@ def set_next_day():
 
 
 if __name__ == '__main__':
-    #set_next_day()
-    change_fake_time()
+    set_next_day()
+    #change_fake_time()
