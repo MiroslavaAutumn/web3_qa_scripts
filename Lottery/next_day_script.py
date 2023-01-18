753a4f68f57e83ac526bb439699c051f12c5f751
miroslavaautunm <miroslavaautumn@gmail.com>
2023-01-18 14:16:21 +0300

    Added changeFakeTimeListen method

--- Lottery/next_day_script.py
+++ Lottery/next_day_script.py
@@ -2,19 +2,32 @@ import Lottery.preset
 from Lottery.preset import sign_send_tx, web3_init, get_contract_abi
 
 
-def set_next_day():
+def lottery_init():
     web3 = web3_init()
-    lottery_contract_abi = get_contract_abi(Lottery.preset.LOTTERY)
+    token_abi = get_contract_abi(Lottery.preset.LOTTERY)
     lottery_contract = web3.eth.contract(address=web3.toChecksumAddress(
         Lottery.preset.LOTTERY),
-        abi=lottery_contract_abi)
+        abi=token_abi)
+    return lottery_contract, web3
+
+
+def change_fake_time():
+    lottery_contract, web3 = lottery_init()
+
+    change_fake_time_listen = lottery_contract.functions.changeFakeTimeListen()
+    tx_hash = sign_send_tx(web3, change_fake_time_listen, Lottery.preset.OWNER,
+                           Lottery.preset.OWNER_PRIV)
+    print('switch', 'https://testnet.bscscan.com/tx/' + tx_hash.hex())
+
+
+def set_next_day():
+    lottery_contract, web3 = lottery_init()
 
     get_current_timestamp = lottery_contract.functions.fakeTimestamp().call()
     next_day = int(get_current_timestamp) + 86400
 
     change_fake_timestamp = lottery_contract.functions.changeFakeTimestamp(
         next_day
-
     )
     tx_hash = sign_send_tx(web3, change_fake_timestamp, Lottery.preset.OWNER,
                            Lottery.preset.OWNER_PRIV)
@@ -22,4 +35,5 @@ def set_next_day():
 
 
 if __name__ == '__main__':
-    set_next_day()
+    #set_next_day()
+    change_fake_time()
