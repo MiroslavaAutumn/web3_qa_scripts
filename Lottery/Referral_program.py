79c2d5cb4986178669643f50b00c83ffaae79f9f
miroslavaautunm <miroslavaautumn@gmail.com>
2023-01-31 17:28:06 +0300

    small fixes for multiple referral code request

--- Lottery/Referral_program.py
+++ Lottery/Referral_program.py
@@ -3,28 +3,25 @@ from Lottery.preset import web3_init
 import Lottery.secret
 
 
-def get_factory_ref_code():
+def get_factory_ref_code(address=Lottery.secret.USER_1, priv=Lottery.secret.USER_1_PRIV, amount=0.00888):
     web3 = web3_init()
 
-    address = Lottery.secret.USER
     nonce = web3.eth.getTransactionCount(address)
-
-    amount = 0.00888
 
     # build transaction
     tx = {
         'nonce': nonce,
-        'to': Lottery.preset.FACTORY,
+        'to': Lottery.preset.FACTORY_PROXY,
         'value': web3.toWei(amount, 'ether'),
         'gas': 1000000,
         'gasPrice': web3.toWei('10', 'gwei')
     }
-    signed_tx = web3.eth.account.signTransaction(tx, Lottery.secret.USER_PRIV)
+    signed_tx = web3.eth.account.signTransaction(tx, priv)
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
     print('FACTORY Ref code', 'https://testnet.bscscan.com/tx/' + tx_hash.hex())
 
 
-def get_lottery_ref_code(address=Lottery.secret.USER, priv=Lottery.secret.USER_PRIV, amount=0.00888):
+def get_lottery_ref_code(address=Lottery.secret.USER_1, priv=Lottery.secret.USER_1_PRIV, amount=0.00888):
     web3 = web3_init()
     nonce = web3.eth.getTransactionCount(address)
 
@@ -45,12 +42,8 @@ def multiple_get_ref_code():
     amount = 0.00888
 
     users = {
-        Lottery.secret.OWNER: {
-            'priv': Lottery.secret.OWNER_PRIV,
-            'amount': amount
-        },
-        Lottery.secret.USER: {
-            'priv': Lottery.secret.USER_PRIV,
+        Lottery.secret.USER_1: {
+            'priv': Lottery.secret.USER_1_PRIV,
             'amount': amount
         },
         Lottery.secret.USER_2: {
@@ -80,12 +73,24 @@ def multiple_get_ref_code():
         Lottery.secret.USER_8: {
             'priv': Lottery.secret.USER_8_PRIV,
             'amount': amount
+        },
+        Lottery.secret.USER_9: {
+            'priv': Lottery.secret.USER_9_PRIV,
+            'amount': amount
+        },
+        Lottery.secret.USER_10: {
+            'priv': Lottery.secret.USER_10_PRIV,
+            'amount': amount
+        },
+        Lottery.secret.USER_11: {
+            'priv': Lottery.secret.USER_11_PRIV,
+            'amount': amount
         }
     }
 
     for user, values in users.items():
-        get_lottery_ref_code(user, values.get('priv'), values.get('amount'))
-        # get_factory_ref_code(user, values.get('priv'), values.get('amount'))
+        #get_lottery_ref_code(user, values.get('priv'), values.get('amount'))
+        get_factory_ref_code(user, values.get('priv'), values.get('amount'))
 
 
 if __name__ == '__main__':
